a00d77bf581b4d7aa627cce672e19a1d4b7a89a4
Roy <shikhorroy@gmail.com>
2021-08-21 22:14:02 +0600

    Add String Permutation

---
+++ src/roy/coder/utils/algo/math/suffling/StrPermutation.java
@@ -0,0 +1,70 @@
+package roy.coder.utils.algo.math.suffling;
+
+import roy.coder.utils.utility.Swap;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class StrPermutation {
+
+    private final char[] chars;
+    private final Set<String> stringSet;
+    private final List<String> stringList;
+
+    public StrPermutation(String string) {
+        chars = string.toCharArray();
+
+        stringSet = new HashSet<>();
+        stringList = new ArrayList<>();
+    }
+
+    /**
+     * Generate all permutation list (duplicate elements may found).
+     *
+     * @return all possible permutation.
+     */
+    public StrPermutation permute() {
+        if (this.count() == 0) this.permute(0, this.chars.length - 1);
+        return this;
+    }
+
+    private void permute(int leftIndex, int rightIndex) {
+        if (leftIndex == rightIndex) {
+            stringList.add(new String(chars));
+        } else {
+            for (int i = leftIndex; i <= rightIndex; i++) {
+                chars[i] = Swap.between(chars[leftIndex], chars[leftIndex] = chars[i]);
+                this.permute(leftIndex + 1, rightIndex);
+                chars[i] = Swap.between(chars[leftIndex], chars[leftIndex] = chars[i]);
+            }
+        }
+    }
+
+    public StrPermutation unique() {
+        if (this.countDistinct() == 0) {
+            this.permute();
+            this.stringSet.addAll(this.stringList);
+        }
+        return this;
+    }
+
+    public List<String> list() {
+        this.permute();
+        return this.stringList;
+    }
+
+    public Set<String> set() {
+        this.unique();
+        return this.stringSet;
+    }
+
+    public int count() {
+        return this.stringList.size();
+    }
+
+    public int countDistinct() {
+        return this.stringSet.size();
+    }
+}
