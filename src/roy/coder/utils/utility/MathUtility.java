e1dbd5aa5584305fc21a5dd65665f7f4749409e6
Roy <shikhorroy@gmail.com>
2021-08-30 13:15:42 +0600

    Polish MathUtility

--- src/roy/coder/utils/utility/MathUtility.java
+++ src/roy/coder/utils/utility/MathUtility.java
@@ -2,28 +2,19 @@ package roy.coder.utils.utility;
 
 public class MathUtility {
 
-    public static boolean isPerfectSquare(long num) {
-        long squareRoot = (long) Math.sqrt(num);
-        return squareRoot * squareRoot == num;
+    public static <T> boolean isOdd(T number) {
+        long longNumber = Long.parseLong(number.toString());
+        return ((longNumber & 1) > 0);
     }
 
-    public static boolean isPerfectSquare(int num) {
-        return isPerfectSquare((long) num);
+    public static <T> boolean isEven(T number) {
+        long longNumber = Long.parseLong(number.toString());
+        return ((longNumber & 1) == 0);
     }
 
-    public static boolean isEven(int number) {
-        return isEven((long) number);
-    }
-
-    public static boolean isEven(long number) {
-        return ((number & 1) == 0);
-    }
-
-    public static boolean isOdd(int number) {
-        return isOdd((long) number);
-    }
-
-    public static boolean isOdd(long number) {
-        return ((number & 1) > 0);
+    public static <T> boolean isPerfectSquare(T number) {
+        long longNumber = Long.parseLong(number.toString());
+        long squareRoot = (long) Math.sqrt(longNumber);
+        return squareRoot * squareRoot == longNumber;
     }
 }
