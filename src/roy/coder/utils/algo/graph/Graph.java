f3a379267ec25438113c6484c20c798f3bb2203b
shikhor.kumer <shikhor.kumer@bjitgroup.com>
2022-07-11 21:52:45 +0600

    Update Graph Class.
    - New childrenOf method added.

--- src/roy/coder/utils/algo/graph/Graph.java
+++ src/roy/coder/utils/algo/graph/Graph.java
@@ -3,6 +3,11 @@ package roy.coder.utils.algo.graph;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Representation of graph with adjacency list.
+ * Nodes are zero-index based, e.g. if Graph initiated with n nodes,
+ * nodes are 0, 1, 2, ..... n - 1.
+ */
 public class Graph {
 
     public enum Type {
@@ -38,5 +43,16 @@ public class Graph {
     public void addEdge(int source, int destination) {
         this.adjacencyList.get(source).add(destination);
         if (this.type.equals(Type.BIDIRECTIONAL)) this.adjacencyList.get(destination).add(source);
+    }
+
+    /**
+     * Give the children of node.
+     *
+     * @param node asking for which children.
+     * @return List of all child.
+     */
+    public List<Integer> childrenOf(int node) {
+        if (node < this.getNodesCount()) return this.adjacencyList.get(node);
+        throw new AssertionError("node out of bound");
     }
 }
