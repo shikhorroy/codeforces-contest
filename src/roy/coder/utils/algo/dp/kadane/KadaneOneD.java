c9914ac58d1c691c91db8bdf27942a215ab4c898
shikhor.kumer <shikhor.kumer@bjitgroup.com>
2022-07-29 23:58:06 +0600

    [ALG][UPD] Update Kadane's Code

--- src/roy/coder/utils/algo/dp/kadane/KadaneOneD.java
+++ src/roy/coder/utils/algo/dp/kadane/KadaneOneD.java
@@ -7,58 +7,46 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Algorithm - Kadane's algorithm: Find the max sub-array sum of one dimensional array.
- * <p>
- * Complexity: O(n)
- */
-public class KadaneOneD<T extends Number & Comparable<T>> {
+public class KadaneOneD {
 
-    public T maxSum;
-    private final List<T> list;
+    public int maxSum;
+    private final List<Integer> list;
     public List<Pair<Integer, Integer>> ranges;
 
-    public KadaneOneD(List<T> list) {
+    public KadaneOneD(List<Integer> list) {
         ranges = new ArrayList<>();
-        maxSum = (T) Integer.valueOf(0);
         this.list = list != null ? list : new ArrayList<>();
+
+        run();
     }
 
-    public T run() {
-        if (list.size() == 0) return (T) Integer.valueOf(0);
+    private void run() {
+        int start = 0, end = 0;
+        int currMaxSum = list.get(0), maxSumSoFar = list.get(0);
 
-        int start = 1, end = 1;
-        T currMaxSum = list.get(0), maxSumSoFar = list.get(0);
-
-        Map<T, List<Pair<Integer, Integer>>> valueRanges = new HashMap<>();
+        Map<Integer, List<Pair<Integer, Integer>>> valueRanges = new HashMap<>();
         valueRanges.put(currMaxSum, new ArrayList<>());
         valueRanges.get(currMaxSum).add(Pair.of(start, end));
 
         for (int i = 1, sz = list.size(); i < sz; i++) {
-            T currValue = list.get(i);
-            T sum = this.sum(currMaxSum, currValue);
+            int currValue = list.get(i);
+            int sum = currMaxSum + currValue;
 
-            if (currValue.compareTo(sum) > 0) {
+            if (currValue > sum) {
                 currMaxSum = currValue;
-                start = end = i + 1;
+                start = end = i;
             } else {
                 currMaxSum = sum;
-                end = i + 1;
+                end = i;
             }
 
-            if (currMaxSum.compareTo(maxSumSoFar) >= 0) {
+            if (currMaxSum >= maxSumSoFar) {
                 maxSumSoFar = currMaxSum;
                 if (!valueRanges.containsKey(maxSumSoFar)) valueRanges.put(maxSumSoFar, new ArrayList<>());
                 valueRanges.get(maxSumSoFar).add(Pair.of(start, end));
             }
         }
         ranges = valueRanges.get(maxSumSoFar);
-        return maxSum = maxSumSoFar;
-    }
-
-    private T sum(T x, T y) {
-        if (x instanceof Integer) return (T) Integer.valueOf(x.intValue() + y.intValue());
-        if (x instanceof Long) return (T) Long.valueOf(x.longValue() + y.longValue());
-        return (T) Double.valueOf(x.doubleValue() + y.doubleValue());
+        maxSum = maxSumSoFar;
     }
 }
